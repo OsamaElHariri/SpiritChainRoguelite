6cd865c99555b4014a5c501022082c874ab300ab
Osama El Hariri <yamsandbread@gmail.com>
2019-10-27 14:23:25 +0200

    Add back button to videos scene

--- src/scenes/VideosScene.ts
+++ src/scenes/VideosScene.ts
@@ -12,9 +12,11 @@ export class VideosScene extends Scene {
     create(): void {
         this.add.rectangle(0, 0, 800, 600, 0xa4a4a4).setOrigin(0);
         this.add.rectangle(0, 0, 800, 100, 0xffffff).setOrigin(0);
-        this.add.sprite(36.5, 64, 'phonebackicon').setOrigin(0, 0.5);
+        this.add.sprite(36.5, 64, 'phonebackicon').setOrigin(0, 0.5).setInteractive().on('pointerdown', () => {
+            this.scene.stop('VideosScene');
+        });
         this.add.sprite(100, 64, 'trendingvideosicon').setOrigin(0, 0.5);
-        this.add.text(140, 64, 'Trending', { color: '#4e4e4e', fontSize: '42px' }).setOrigin(0, 0.5);
+        this.add.text(144, 64, 'Trending', { color: '#4e4e4e', fontSize: '42px' }).setOrigin(0, 0.5);
         new PhoneHeaderBar(this, 0, 0);
         this.videos.push(new PhoneVideoPanel(this, 0, 105));
         this.videos.push(new PhoneVideoPanel(this, 0, 270));
