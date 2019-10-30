db227f156f53a804c2723d671259c75d6ba80db7
Osama El Hariri <yamsandbread@gmail.com>
2019-10-30 20:20:32 +0200

    Add depth to actor

--- src/actors/Actor.ts
+++ src/actors/Actor.ts
@@ -34,6 +34,7 @@ export class Actor extends Phaser.GameObjects.Ellipse {
         this.id = world.scene.addObject(this);
         world.scene.physics.world.enable(this);
         this.body.setAllowGravity(false);
+        this.depth = 10;
         this.container = world.scene.add.container(x, y);
         this.healthBar = this.constructHealthBar();
         this.container.add(this.healthBar);
